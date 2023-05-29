6d4e3ebaa9a5fc26ff72e24c8580a447ee5eaf75
Djamalutin <fisher.gerryy@mail.ru>
2023-05-29 15:02:14 +0300

    feat: useForm using

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  console.log(errors);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      style={{ padding: "40px" }}
+      onSubmit={handleSubmit((data) => console.log(data, "submitting data"))}
+    >
+      <label>firstName</label>
+      <tr></tr>
+      <input {...register("firstName", { required: true })} />
+      {errors.firstName && <span>This field is required</span>}
+      <tr></tr>
+      <label>secondName</label>
+      <tr></tr>
+      <input {...register("secondName", {})} />
+      <tr></tr>
+      <label>age</label>
+      <tr></tr>
+      <input {...register("age", { required: true, valueAsNumber: true })} />
+      {errors.age && <span>This field is required</span>}
+      <tr></tr>
+      <label>gender</label>
+      <tr></tr>
+      <input {...register("gender", { required: true })} />
+      {errors.gender && <span>This field is required</span>}
+      <tr></tr>
+      <input type="submit" />
+    </form>
   );
 }
 
