4e9cc586d63abe65e222662eff074c9a2e58f7d2
Djamalutin <fisher.gerryy@mail.ru>
2023-05-29 16:18:20 +0300

    register and required

--- src/App.js
+++ src/App.js
@@ -16,22 +16,47 @@ function App() {
     >
       <label>firstName</label>
       <tr></tr>
-      <input {...register("firstName", { required: true })} />
-      {errors.firstName && <span>This field is required</span>}
+      <input
+        {...register("firstName", {
+          required: "This is Required!",
+          maxLength: {
+            value: 25,
+            message: `You are exeeded 25 symbols!`,
+          },
+        })}
+      />
+      {errors.firstName && <span>{errors.firstName.message}</span>}
       <tr></tr>
       <label>secondName</label>
       <tr></tr>
-      <input {...register("secondName", {})} />
+      <input
+        {...register("secondName", {
+          required: "This is Required!",
+          maxLength: {
+            value: 25,
+            message: `You are exeeded 25 symbols!`,
+          },
+        })}
+      />
+      {errors.secondName && <span>{errors.secondName.message}</span>}
       <tr></tr>
       <label>age</label>
       <tr></tr>
-      <input {...register("age", { required: true, valueAsNumber: true })} />
-      {errors.age && <span>This field is required</span>}
+      <input
+        {...register("age", {
+          required: "This is Required!",
+          valueAsNumber: "this option has been a Number",
+        })}
+      />
+      {errors.age && <span>{errors.age.message}</span>}
       <tr></tr>
       <label>gender</label>
       <tr></tr>
-      <input {...register("gender", { required: true })} />
-      {errors.gender && <span>This field is required</span>}
+      <select {...register("gender", { required: "This is Required!" })}>
+        <option value="male">male</option>
+        <option value="female">female</option>
+      </select>
+      {errors.gender && <span>{errors.gender.message}</span>}
       <tr></tr>
       <input type="submit" />
     </form>
